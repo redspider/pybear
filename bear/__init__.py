redspider/pybear

commit c2b0b611cefe815d619f90edbbdd3e90365ee65b

bear: add Bear.get_note()
Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>
Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

bear/__init__.py
@@ -180,6 +180,15 @@ class Bear(object):
             text = row['ZTEXT']
         )
 
+    def get_note(self, id):
+        cursor = self._db.cursor()
+        cursor.execute(
+            'SELECT * FROM ZFSNOTE WHERE ZUNIQUEIDENTIFIER = ?', [id])
+        row = cursor.fetchone()
+        if not row:
+            return None
+        return self._row_to_note(row)
+
     def tags(self):
         """
         CREATE TABLE ZSFNOTETAG ( Z_PK INTEGER PRIMARY KEY, Z_ENT INTEGER, Z_OPT INTEGER, ZMODIFICATIONDATE 

commit 7ce11211dafb3064d299233a88d784f2ef00a5c5

Fix typo in SQL table name
This was introduced in commit c2b0b611cefe815d619f90edbbdd3e90365ee65b
as part of MR #5.

Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

bear/__init__.py
@@ -246,7 +246,7 @@ class Bear(object):
     def get_note(self, id):
         cursor = self._db.cursor()
         cursor.execute(
-            'SELECT * FROM ZFSNOTE WHERE ZUNIQUEIDENTIFIER = ?', [id])
+            'SELECT * FROM ZSFNOTE WHERE ZUNIQUEIDENTIFIER = ?', [id])
         row = cursor.fetchone()
         if not row:
             return None